47999add4c959623d5aba4d0edf469e21ccb198f
CamilleSilene <trillatcam@gmail.com>
2024-08-12 22:00:45 +0200

    apropos-collapse

--- src/pages/Apropos/Apropos.jsx
+++ src/pages/Apropos/Apropos.jsx
@@ -1,10 +1,24 @@
-import Banner from "../../components/Agencement/Banner"
-import imgBannerAbout from "../../assets/BannerAbout.png"
+import Banner from "../../components/Agencement/Banner";
+import imgBannerAbout from "../../assets/BannerAbout.png";
+import Collapse from "../../components/Collapse/collapse";
+import aproposJson from "../../data/apropos";
+import './apropos.scss';
 
 const Apropos = () => {
     return (
         <div>
             <Banner bgImage={imgBannerAbout} alt="montagne et forêt"/>
+            <div className="containerCollapseApropos">            
+            
+            {aproposJson.map((item, index) => {
+                
+            
+              return <Collapse key={index} titre={item.title}>{item.description}</Collapse> }
+              
+            )}
+            
+            </div>
+            
         </div>
     )
 }
--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,60 +1,66 @@
 import { Navigate, useParams } from "react-router-dom";
-import logementsJson from '../../data/logements';
-import './logement.scss';
+import logementsJson from "../../data/logements";
+import "./logement.scss";
 import Collapse from "../../components/Collapse/collapse";
 
-
 const Location = () => {
-    const { id } = useParams();
-    const logements = logementsJson.filter((l) => l.id === id );
-    if(logements.length === 0) {
-        return <Navigate to ="/Error"/>
-    }
+  const { id } = useParams();
+  const logements = logementsJson.filter((l) => l.id === id);
+  if (logements.length === 0) {
+    return <Navigate to="/Error" />;
+  }
 
-    const logement = logements[0];
-    return (
-        <div>
-            <div>Caroussel</div>
-            <h1>{logement.title}</h1>
-            <div>{logement.host.name} - <img src={logement.host.picture} alt="host" /></div>
-            <span>{logement.location}</span>
-            <p>{logement.description}</p>
+  const logement = logements[0];
+  return (
+    <div>
+      <div>Caroussel</div>
+      <h1>{logement.title}</h1>
 
-            <div>
-            {logement.tags.map( (tag, index) => { 
-                return ( <span className="badge" key={index} >{index} {tag}</span>)
+      <div className="HostLogement">
+        {logement.host.name} - <img src={logement.host.picture} alt="host" />
+      </div>
+
+      <div className="LocationLogement">
+      <span>{logement.location}</span>
+      </div>
+
+      <div className="DescriptionLogement">
+      <p>{logement.description}</p>
+      </div>
+
+      <div className="TagLogement">
+        {logement.tags.map((tag, index) => {
+          return (
+            <span className="badge" key={index}>
+              {index} {tag}
+            </span>
+          );
+        })}
+      </div>
+
+      <div className="starLogement">
+      {Array.from(Array(5), (e, index) => {
+        const starClass =
+          index < logement.rating ? "fa-star-active" : "fa-star-inactive";
+        const starClasses = `fa-solid fa-star ${starClass}`;
+        return <i key={index} className={starClasses}></i>;
+      })}
+      </div>
+
+      <div className="collapsesWrapperLogement">
+        <Collapse titre="Description">
+          <p>{logement.description}</p>
+        </Collapse>
+        <Collapse titre="Equipements">
+          <ul>
+            {logement.equipments.map((equipment, index) => {
+              return <li key={index}> {equipment}</li>;
             })}
-            </div>
+          </ul>
+        </Collapse>
+      </div>
+    </div>
+  );
+};
 
-            <div>Composant Rating : {logement.rating}</div>
-            {
-                Array.from(Array(5), (e,i) => {
-                    const starClass = i < logement.rating ? "fa-star-active" : "fa-star-inactive";
-                    const starClasses = `fa-solid fa-star ${starClass}`
-                    return (
-                        <i key={i} className={starClasses}></i>
-                    )
-                })
-            }
-            <div>Accordeon Description (paragraphe) 
-                <Collapse titre="Description">
-                    <p>
-                    {logement.description}
-                    </p>
-                </Collapse> 
-                <Collapse titre="Equipement">
-                    <ul>
-                        <li>Item 1</li>
-                        <li>Item 2</li>
-                        <li>Item 3</li>
-                        <li>Item 4</li>
-                    </ul>
-                </Collapse>
-            </div>
-            <div>Accordeon Equipements (liste)</div>
-        </div>
-    )
-}
-
-export default Location 
-
+export default Location;
