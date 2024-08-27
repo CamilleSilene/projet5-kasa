7a9e008a55cd03bbd564460063ce5f449bbe9ad9
CamilleSilene <trillatcam@gmail.com>
2024-08-27 20:25:25 +0200

    fix collapse animation at start

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -5,17 +5,27 @@ const Collapse = (props) => {
     //initialement c'est à false car il faut pouvoir faire apparaître le texte
 
     const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isInitialCollapsed, setIsInitialCollapsed] = useState(true);
     //si l'élément est étendu alors on lui ajoute la classe 'collapsed'    
     //création de toutes les classes qui vont aller dans l'élément
     // 'collapse' et 'collapsed' 
 
-    const classes = (props.cssClasses !== undefined ? props.cssClasses : '' ) + " collapse" + ( isCollapsed ? ' collapsed' : '' );
+    const classes = 
+    (props.cssClasses !== undefined ? props.cssClasses : '' ) + 
+    " collapse" 
+    + ( isCollapsed ? ' collapsed' : '' )
+    + ( isInitialCollapsed ? ' initial-collapsed' : '' )
+    
+    
     return (
         <div className={classes}>
             <div className='collapse-header background-primary'>
                 <span className='collapse-title text-tertiary'>{props.titre}</span>
                 <button className='background-primary text-tertiary' onClick={() => { 
                     setIsCollapsed(!isCollapsed);
+                    if(isInitialCollapsed) {
+                        setIsInitialCollapsed(false);
+                    }
                     } }>
                     <div className="fa-solid fa-chevron-up"></div>
                 </button>
